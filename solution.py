11d28632c35868b6f9365d766c5ef5289430fed9
iraira <98972208+IraNovichkova@users.noreply.github.com>
2023-04-13 16:02:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def solution(x: np.array, y: np.array) -> bool:
     x = x.ravel()
     y = y.ravel()
     p = 0.06
-    if (stats.ks_2samp(x, y).pvalue > p):
+    if (stats.anderson_ksamp([x, y]) > p:
         return False
     else:
         return True
