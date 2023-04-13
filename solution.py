ef35260d52d8909fd7d0b3c37d86c313019da3b5
iraira <98972208+IraNovichkova@users.noreply.github.com>
2023-04-13 22:51:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,6 @@ from hyppo.ksample import Energy, MMD
 chat_id = 335933917
 
 def solution(x: np.array, y: np.array) -> bool:
-    x = x.ravel()
-    y = y.ravel()
     p = 0.06
     if (stats.cramervonmises_2samp(x, y).pvalue >= p):
         return False
