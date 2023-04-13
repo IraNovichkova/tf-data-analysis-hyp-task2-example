3462a6e6abc24c9fb516298e4de0811652548c62
iraira <98972208+IraNovichkova@users.noreply.github.com>
2023-04-13 22:46:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+from hyppo.ksample import Energy, MMD
 
 
 chat_id = 335933917
@@ -9,7 +10,7 @@ def solution(x: np.array, y: np.array) -> bool:
     x = x.ravel()
     y = y.ravel()
     p = 0.06
-    if (stats.anderson_ksamp([x, y]).pvalue > p):
+    if (stats.cramervonmises_2samp(x, y).pvalue >= p):
         return False
     else:
         return True
